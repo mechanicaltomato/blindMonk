b30c168241bd4c24ee63ef64051e667c2daa09c6
RFPadilha <34008458+RFPadilha@users.noreply.github.com>
2020-02-18 18:59:24 -0300

    Integrated enemy and level lists
    fixed bugs that came with it, changed skill point usage

---
+++ EnemyList.js
@@ -0,0 +1,803 @@
+let enemyList = [
+  {
+    order: '1',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '2',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '3',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '4',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '6',
+    boss: 'FALSE'
+  },
+  {
+    order: '5',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '6',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '7',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '8',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '9',
+    name: 'Tiny Slime',
+    hp: '10',
+    room: '1',
+    expValue: '1',
+    boss: 'FALSE'
+  },
+  {
+    order: '10',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'TRUE'
+  },
+  {
+    order: '11',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '12',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '13',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '14',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '15',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '16',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '17',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '18',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '19',
+    name: 'Gooey Slime',
+    hp: '26',
+    room: '2',
+    expValue: '2',
+    boss: 'FALSE'
+  },
+  {
+    order: '20',
+    name: 'Stinky Slime',
+    hp: '26',
+    room: '3',
+    expValue: '2',
+    boss: 'TRUE'
+  },
+  {
+    order: '21',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '22',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '23',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '24',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '25',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '26',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '27',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '28',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '29',
+    name: 'Stinky Slime',
+    hp: '47',
+    room: '3',
+    expValue: '3',
+    boss: 'FALSE'
+  },
+  {
+    order: '30',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'TRUE'
+  },
+  {
+    order: '31',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '32',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '33',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '34',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '35',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '36',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '37',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '38',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '39',
+    name: 'Regular Slime',
+    hp: '78',
+    room: '4',
+    expValue: '5',
+    boss: 'FALSE'
+  },
+  {
+    order: '40',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'TRUE'
+  },
+  {
+    order: '41',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '42',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '43',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '44',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '45',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '46',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '47',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '48',
+    name: 'Chunky Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'FALSE'
+  },
+  {
+    order: '49',
+    name: 'Boss Slime',
+    hp: '119',
+    room: '5',
+    expValue: '8',
+    boss: 'TRUE'
+  },
+  {
+    order: '50',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '51',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '52',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '53',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '54',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '55',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '56',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '57',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '58',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '59',
+    name: 'Large Slime',
+    hp: '179',
+    room: '6',
+    expValue: '12',
+    boss: 'FALSE'
+  },
+  {
+    order: '60',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'TRUE'
+  },
+  {
+    order: '61',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '62',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '63',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '64',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '65',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '66',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '67',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '68',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '69',
+    name: 'Double Slime',
+    hp: '265',
+    room: '7',
+    expValue: '18',
+    boss: 'FALSE'
+  },
+  {
+    order: '70',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'TRUE'
+  },
+  {
+    order: '71',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '72',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '73',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '74',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '75',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '76',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '77',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '78',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '79',
+    name: 'Triple Slime',
+    hp: '393',
+    room: '8',
+    expValue: '26',
+    boss: 'FALSE'
+  },
+  {
+    order: '80',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'TRUE'
+  },
+  {
+    order: '81',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '82',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '83',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '84',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '85',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '86',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '87',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '88',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '89',
+    name: 'Giant Slime',
+    hp: '586',
+    room: '9',
+    expValue: '39',
+    boss: 'FALSE'
+  },
+  {
+    order: '90',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'TRUE'
+  },
+  {
+    order: '91',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '92',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '93',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '94',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '95',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '96',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '97',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '98',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '99',
+    name: 'Slime Army',
+    hp: '880',
+    room: '10',
+    expValue: '59',
+    boss: 'FALSE'
+  },
+  {
+    order: '100',
+    name: 'Supreme Slime',
+    hp: '880',
+    room: '10',
+    expValue: '71',
+    boss: 'TRUE'
+  }
+];
+
